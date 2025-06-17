ff84c3f82078fdf51563e0fa0bdc355f4539abbe
yalidhan <dedek.southborneo@gmail.com>
2025-06-17 13:54:49 +0800

    fixing table perencaan

--- resources/views/planning/planning.blade.php
+++ resources/views/planning/planning.blade.php
@@ -573,5 +573,21 @@ $(document).ready(function () {
         }
     });
 });
+$(document).ready(function () {
+    // Initialize DataTable
+    var table = $('#plannings3').DataTable({
+        "order": [],
+        initComplete: function () {
+            this.api().columns().every(function () {
+                var column = this;
+                $('input', this.footer()).on('keyup change', function () {
+                    if (column.search() !== this.value) {
+                        column.search(this.value).draw();
+                    }
+                });
+            });
+        }
+    });
+});
 </script>
 @endpush
